273ea763978653fadc2894722e0d75ba0bee48ca
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-07-16 23:35:19 +0100

    Finished Feed and Tweet's Component

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -1,11 +1,60 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { 
+Container, 
+Retweeted, 
+RocketseatIcon, 
+Body, 
+Avatar, 
+Content, 
+Header, 
+Dot, 
+Description, 
+ImageContent, 
+Icons, 
+Status, 
+ComentIcon, 
+RetweetIcon, 
+LikeIcon,
+} from './styles';
 
 const Tweet: React.FC = () => {
   return (
     <Container>
-      
+      <Retweeted>
+        <RocketseatIcon />
+        Voce retuitou
+      </Retweeted>
+
+      <Body>
+        <Avatar />
+
+        <Content>
+          <Header>
+            <strong>Daniel</strong>
+            <span>@Daniel </span>
+            <Dot />
+            <time>12 de maio</time>
+          </Header>
+
+          <Description> oi</Description>
+          <ImageContent />
+          <Icons>
+            <Status>
+              <ComentIcon />
+              18
+            </Status>
+            <Status>
+              <RetweetIcon />
+              56 
+            </Status>
+            <Status>
+              <LikeIcon />
+              180
+            </Status>
+          </Icons>
+        </Content>
+      </Body>
     </Container>
   );
 }
